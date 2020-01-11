70d51298bb4634bdf48fcbb9bc706da3f1d1f956
Łukasz Goral <lukasz.goral.design@gmail.com>
2020-01-11 14:59:15 +0100

    first observer

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,41 @@
-import * as Rx from 'rxjs';
+import { Observable } from 'rxjs';
 
-console.log(Rx);
+const addItmem = (val: any) => {
+    const node = document.createElement('li')
+    const textnode = document.createTextNode(val)
+    node.appendChild(textnode)
+    document.getElementById('output').appendChild(node)
+}
+
+const observable = Observable.create((observer: any) => {
+    try {
+        observer.next('Hey guys!')
+        observer.next('How are you?!')
+        setInterval(() => {
+
+            observer.next('FineI am fine')
+            console.log('interval test')
+        }, 2000)
+
+
+    } catch (err) {
+        observer.error(err)
+    }
+});
+
+const observer = observable.subscribe((x: any) => {
+    addItmem(x)
+},
+    (error: any) => { addItmem(error) }
+    ,
+    () => { addItmem('completed') });
+
+ 
+
+   
+
+setTimeout(() => {
+    const observer2 = observable.subscribe((x: any) => {
+        addItmem('Subsriber 2: ' + x)
+    });
+}, 1000)
